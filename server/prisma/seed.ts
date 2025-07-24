6c722a34a2140c1c008fd38d98926ea3c29dd586
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-07-24 22:44:31 +0100

    feat: add track seeding with tags, plays, likes, and comments

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 async function main() {
   const genres = await prisma.genre.createMany({
@@ -32,6 +33,17 @@ async function main() {
       { name: "vocals" },
       { name: "remix" },
       { name: "guitar" },
+      { name: "sematary" },
+      { name: "ghost mountain" },
+      { name: "emo" },
+      { name: "lil peep" },
+      { name: "ivoxygen" },
+      { name: "drain gang" },
+      { name: "jpegmafia" },
+      { name: "death grips" },
+      { name: "ambient" },
+      { name: "lofi" },
+      { name: "deftones" },
     ],
     skipDuplicates: true,
   });
@@ -94,11 +106,246 @@ async function main() {
     skipDuplicates: true,
   });
 
+  // Sample users
+  const hashedPassword =
+    "$2b$08$B9Xpzv0N4fTTwQlALw1mj.YZKOlqzBtZR1ai.4Dwbu4UDs8dxkEbS";
+  const users = [
+    {
+      name: "Daniel",
+      username: "fdanctl",
+      // avatarUrl: "https://i.pravatar.cc/150?img=1",
+      email: "test@test.com",
+      password: hashedPassword,
+      location: "Lisbon",
+    },
+    {
+      name: "Alice Johnson",
+      username: "alicej",
+      avatarUrl: "https://i.pravatar.cc/150?img=1",
+      email: "alice@example.com",
+      password: hashedPassword,
+      location: "New York",
+    },
+    {
+      name: "Bob Smith",
+      username: "bobsmith",
+      avatarUrl: "https://i.pravatar.cc/150?img=2",
+      email: "bob@example.com",
+      password: hashedPassword,
+      location: "Los Angeles",
+    },
+    {
+      name: "Charlie Brown",
+      username: "charlieb",
+      avatarUrl: "https://i.pravatar.cc/150?img=3",
+      email: "charlie@example.com",
+      password: hashedPassword,
+      location: "Chicago",
+    },
+  ];
+
+  const producer2 = await prisma.user.create({
+    data: {
+      name: "Mira Lavoie",
+      username: "DigitalMira",
+      avatarUrl: "https://i.pravatar.cc/150?img=3",
+      email: "mira@example.com",
+      password: hashedPassword,
+      location: "Barcelona",
+    },
+  });
+
+  const usersN = await prisma.user.createMany({
+    data: users.map((data) => data),
+    skipDuplicates: true,
+  });
+
+  const producerX = await prisma.user.create({
+    data: {
+      name: "Producer X",
+      username: "producerx",
+      avatarUrl: "https://i.pravatar.cc/150?img=4",
+      email: "producerx@example.com",
+      password: hashedPassword,
+      location: "Berlin",
+    },
+  });
+
+  const track = await prisma.track.create({
+    data: {
+      name: "LO-FI TYPE BEAT - ONE LOVE",
+      trackUrl: "/upload/track/[tVcwohev7Aw].mp3",
+      imgUrl: "/upload/track/[tVcwohev7Aw].jpg",
+      userId: producerX.id,
+      genreId: 4,
+      moodId: 1,
+      trackTypeId: 1,
+      keyId: 20,
+      bpm: 75,
+      price: 1499,
+      Tag: {
+        connect: [4, 18, 19].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: "bladee x whitearmor x drain gang type beat - solace",
+      trackUrl: "/upload/track/[bYN32e385eA].mp3",
+      imgUrl: "/upload/track/[bYN32e385eA].jpg",
+      userId: producerX.id,
+      genreId: 12,
+      moodId: 4,
+      trackTypeId: 1,
+      keyId: 12,
+      bpm: 140,
+      price: 4999,
+      Tag: {
+        connect: [15].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'Lil Peep x IVOXYGEN "Nostalgia" (Prod. Sanctions x Maestro Wons)',
+      trackUrl: "/upload/track/[BpP_reXvLks].mp3",
+      imgUrl: "/upload/track/[BpP_reXvLks].jpg",
+      userId: producerX.id,
+      genreId: 2,
+      moodId: 4,
+      trackTypeId: 1,
+      keyId: 12,
+      bpm: 130,
+      price: 9999,
+      Tag: {
+        connect: [13, 12, 14].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'Sematary Bloody Angels Type Beat "DIE 4 ME"',
+      trackUrl: "/upload/track/[8yk-0oe0LTM].mp3",
+      imgUrl: "/upload/track/[8yk-0oe0LTM].jpg",
+      userId: producerX.id,
+      genreId: 1,
+      moodId: 1,
+      trackTypeId: 1,
+      keyId: 10,
+      bpm: 140,
+      price: 1999,
+      Tag: {
+        connect: [10, 11].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'earl sweatshirt type beat "DiRT"',
+      trackUrl: "/upload/track/[lxJnz7Rk1_8].mp3",
+      imgUrl: "/upload/track/[lxJnz7Rk1_8].jpg",
+      userId: producerX.id,
+      genreId: 2,
+      moodId: 1,
+      trackTypeId: 1,
+      keyId: 20,
+      bpm: 81,
+      price: 1999,
+      Tag: {
+        connect: [4].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'DEATH GRIPS X JPEGMAFIA TYPE BEAT - "TONE HUNT"',
+      trackUrl: "/upload/track/[SdgZOsV-NAg].mp3",
+      imgUrl: "/upload/track/[SdgZOsV-NAg].jpg",
+      userId: producer2.id,
+      genreId: 2,
+      moodId: 8,
+      trackTypeId: 1,
+      keyId: 20,
+      bpm: 130,
+      price: 4499,
+      Tag: {
+        connect: [16, 17].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'LIL PEEP TYPE BEAT - "YOUR LOVE" | EMO RAP TYPE BEAT',
+      trackUrl: "/upload/track/[SX3cBfkBMP0].mp3",
+      imgUrl: "/upload/track/[SX3cBfkBMP0].jpg",
+      userId: producer2.id,
+      genreId: 1,
+      moodId: 4,
+      trackTypeId: 1,
+      keyId: 20,
+      bpm: 145,
+      price: 4499,
+      Tag: {
+        connect: [16, 17].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  await prisma.track.create({
+    data: {
+      name: 'deftones x alternative rock x heavy shoegaze type beat - "rest"',
+      trackUrl: "/upload/track/[YFkECFkLNzc].mp3",
+      imgUrl: "/upload/track/[YFkECFkLNzc].jpg",
+      userId: producer2.id,
+      genreId: 3,
+      moodId: 8,
+      trackTypeId: 1,
+      keyId: 10,
+      bpm: 120,
+      price: 4499,
+      Tag: {
+        connect: [20].map((tag) => ({ id: tag })),
+      },
+    },
+  });
+
+  // Simulate one play
+  await prisma.trackPlay.create({
+    data: {
+      trackId: track.id,
+      userId: producer2.id,
+    },
+  });
+
+  // One like
+  await prisma.like.create({
+    data: {
+      trackId: track.id,
+      userId: producer2.id,
+    },
+  });
+
+  // One comment
+  await prisma.comment.create({
+    data: {
+      trackId: track.id,
+      userId: producer2.id,
+      content: "This track is pure fire 🔥!",
+    },
+  });
+
   console.log("Seed data inserted.");
   console.log("Genre:\n", genres);
   console.log("Tags:\n", tags);
   console.log("Moods:\n", moods);
   console.log("Track Types:\n", trackTypes);
+  console.log("User:\n", usersN);
   console.log("Keys:\n", keyN);
 }
 
